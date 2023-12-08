5cc00abdcac913d1072b6c78e8eef989fa80347a
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2023-12-09 01:21:53 +0900

    repository

---
+++ lib/infrastructure/repository/books_repository_impl.dart
@@ -0,0 +1,47 @@
+import '../../domain/entity/model/book.dart';
+import '../../domain/entity/model/book_id.dart';
+import '../../domain/entity/model/book_list.dart';
+import '../../domain/repository/books_repository.dart';
+import '../datasource/database/books_database.dart';
+import '../mapper/book_list_mapper.dart';
+import '../mapper/book_mapper.dart';
+
+class BooksRepositoryImpl implements BooksRepository {
+  final BooksDatabase database;
+
+  const BooksRepositoryImpl(this.database);
+
+  @override
+  Future<BookList> getAllBooks() async {
+    var entityList = await database.getAllBooks();
+    return BookListMapper.transformToModel(entityList);
+  }
+
+  @override
+  Future<Book> createBook(
+    String title,
+    String description,
+    String isbn,
+  ) async {
+    var entity = await database.insertBook(
+      BookMapper.transformToNewEntityMap(
+        title,
+        description,
+        isbn,
+      ),
+    );
+    return BookMapper.transformToModel(entity);
+  }
+
+  @override
+  Future<void> updateBook(Book book) async {
+    await database.updateBook(
+      BookMapper.transformToMap(book),
+    );
+  }
+
+  @override
+  Future<void> deleteBook(BookId id) async {
+    await database.deleteBook(id.value);
+  }
+}
