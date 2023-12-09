97e0f0c6c27fbb36027685472da39b6a3e8fcd0f
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2023-12-09 21:22:28 +0900

    fix: ci

--- lib/presentation/pages/index/index_view.dart
+++ lib/presentation/pages/index/index_view.dart
@@ -9,7 +9,7 @@ class IndexView extends StatelessWidget {
   IndexView({super.key});
 
   final _filteredBookListProvider = filteredBookListProvider;
-  final _indexViewProvider = indexViewModelStateNotifierProvider;
+  // final _indexViewProvider = indexViewModelStateNotifierProvider;
 
   @override
   Widget build(BuildContext context) {
