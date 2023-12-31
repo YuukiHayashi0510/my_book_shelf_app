144e352626a656f231f3fd26d77ccbf65bb23faa
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2023-12-31 19:14:02 +0900

    fix: indexわかりやすく, ConsumerWidgetに変更

--- lib/presentation/pages/book/index/index_view.dart
+++ lib/presentation/pages/book/index/index_view.dart
@@ -5,13 +5,12 @@ import '../../../viewmodel/book/book_index_viewmodel.dart';
 import 'add_floating_action_button.dart';
 import 'book_card.dart';
 
-class IndexView extends StatelessWidget {
-  IndexView({super.key});
-
-  final _filteredBookListProvider = filteredBookListProvider;
+class BookIndexView extends ConsumerWidget {
+  const BookIndexView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    var filteredBookList = ref.watch(filteredBookListProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('書籍一覧'),
@@ -19,33 +18,29 @@ class IndexView extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Consumer(
-            builder: ((context, ref, _) {
-              return ref.watch(_filteredBookListProvider).maybeWhen(
-                    success: (bookList) => Expanded(
-                      child: bookList.length == 0
-                          ? const Center(
-                              child: Text('本がありません'),
-                            )
-                          : ListView.builder(
-                              itemCount: bookList.length,
-                              shrinkWrap: true,
-                              physics: const NeverScrollableScrollPhysics(),
-                              itemBuilder: (context, index) =>
-                                  BookCard(book: bookList[index]),
-                            ),
+          filteredBookList.maybeWhen(
+            success: (bookList) => Expanded(
+              child: bookList.length == 0
+                  ? const Center(
+                      child: Text('本がありません'),
+                    )
+                  : ListView.builder(
+                      itemCount: bookList.length,
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) =>
+                          BookCard(book: bookList[index]),
                     ),
-                    error: (_) => const Center(
-                      child: Text('エラーが発生しました'),
-                    ),
-                    orElse: () => const Expanded(
-                      child: Center(
-                        child: CircularProgressIndicator(),
-                      ),
-                    ),
-                  );
-            }),
-          )
+            ),
+            error: (_) => const Center(
+              child: Text('エラーが発生しました'),
+            ),
+            orElse: () => const Expanded(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          ),
         ],
       ),
       floatingActionButton: const AddBookFloatingActionButton(),
--- lib/presentation/pages/series/index/index_view.dart
+++ lib/presentation/pages/series/index/index_view.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../viewmodel/series/series_index_viewmodel.dart';
 import 'series_card.dart';
 
-class IndexView extends StatelessWidget {
-  IndexView({Key? key}) : super(key: key);
-
-  final _sortedSeriesListProvider = sortedSeriesListProvider;
+class SeriesIndexView extends ConsumerWidget {
+  const SeriesIndexView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    var sortedSeriesList = ref.watch(sortedSeriesListProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('本棚'),
@@ -18,31 +18,29 @@ class IndexView extends StatelessWidget {
       // TODO: シリーズの冊数に応じて本棚のように表示するUI
       body: Column(
         children: [
-          Consumer(builder: ((context, ref, _) {
-            return ref.watch(_sortedSeriesListProvider).maybeWhen(
-                  success: (seriesList) => Expanded(
-                    child: seriesList.length == 0
-                        ? const Center(
-                            child: Text('シリーズはありません'),
-                          )
-                        : ListView.builder(
-                            itemCount: seriesList.length,
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemBuilder: (context, index) =>
-                                SeriesCard(series: seriesList[index]),
-                          ),
-                  ),
-                  error: (_) => const Center(
-                    child: Text('エラーが発生しました'),
-                  ),
-                  orElse: () => const Expanded(
-                    child: Center(
-                      child: CircularProgressIndicator(),
+          sortedSeriesList.maybeWhen(
+            success: (seriesList) => Expanded(
+              child: seriesList.length == 0
+                  ? const Center(
+                      child: Text('シリーズはありません'),
+                    )
+                  : ListView.builder(
+                      itemCount: seriesList.length,
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) =>
+                          SeriesCard(series: seriesList[index]),
                     ),
-                  ),
-                );
-          }))
+            ),
+            error: (_) => const Center(
+              child: Text('エラーが発生しました'),
+            ),
+            orElse: () => const Expanded(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          )
         ],
       ),
       // TODO: 追加ボタンを押したら、シリーズを追加できるようにする
